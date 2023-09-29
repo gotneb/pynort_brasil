15947e7a8b4488cbdb72bacba7ae04fd64074b08
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-09-29 09:23:53 -0400

    fix: instagram has changed its CSS Selectors

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from selenium.webdriver.common.by import By
 def get_profile_metadata(driver: Firefox, url: str):
     driver.get(url)
 
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(4)
 
-    elem = driver.find_element(By.CSS_SELECTOR, 'section.x972fbf h2')
+    elem = driver.find_element(By.CSS_SELECTOR, 'section.x1qjc9v5 h2') # changed!
     name = elem.text
 
     elems = driver.find_elements(By.CSS_SELECTOR, 'section ul.x78zum5.x1q0g3np.xieb3on li span._ac2a span')
@@ -18,7 +18,7 @@ def get_profile_metadata(driver: Firefox, url: str):
     elem = driver.find_element(By.CSS_SELECTOR, 'section div.x7a106z')
     bio = elem.text
 
-    elem = driver.find_element(By.CSS_SELECTOR, 'img.xav7gou')
+    elem = driver.find_element(By.CSS_SELECTOR, 'img.xpdipgo') # changed
     img = elem.get_attribute('src')
 
     print(f'Name: {name}\nImg: {img[:len(img)//2]}\nPosts: {posts} | Followers: {followers} | Following: {following}\nBio:\n{bio}')
@@ -56,11 +56,11 @@ options = FirefoxOptions()
 # options.add_argument('-headless')
 driver = Firefox(options=options)
 
-url_perfil = 'https://www.instagram.com/bbcbrasil/'
-get_profile_metadata(driver, url_perfil)
+# url_perfil = 'https://www.instagram.com/bbcbrasil/'
+# get_profile_metadata(driver, url_perfil)
 
-# url_post = 'https://www.instagram.com/p/CxGrAqgJoQX/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=='
-# get_post_metadata(driver, url_post)
+url_post = 'https://www.instagram.com/p/CxGrAqgJoQX/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=='
+get_post_metadata(driver, url_post)
 
 input('Press any key to close...')
 driver.close()
